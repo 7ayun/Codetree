8a4ad1f8e5b5a9e52770519b1ccb10579028253e
7ayun <wcentipede27@gmail.com>
2026-02-02 08:42:25 +0900

    Title: 특정 조건 세 정수 비교, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260202/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,11 @@
+a, b, c = map(int, input().split())
+
+if a == b or a == c:
+    print(1, end = ' ')
+else:
+    print(0, end = ' ')
+
+if a == b == c:
+    print(1, end = ' ')
+else:
+    print(0, end = ' ')
