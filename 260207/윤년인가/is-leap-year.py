08c5dc7bc9f08f8937f195d6a856a5d07d37233d
7ayun <wcentipede27@gmail.com>
2026-02-07 19:14:48 +0900

    Title: 윤년인가, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260207/윤년인가/is-leap-year.py
+++ 260207/윤년인가/is-leap-year.py
@@ -1,7 +1,6 @@
 Y = int(input())
 
-if Y % 4 == 0:
+if (Y % 4 == 0 and Y % 100 != 0) or (Y % 400 == 0):
     print('true')
 else:
-    if Y % 4 == 1 or (Y % 100 == 0 and Y % 400 != 0):
-        print('false')
+    print('false')
