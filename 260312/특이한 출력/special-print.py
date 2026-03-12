ff252cff5857084d48df8fa1f6d629160b6f7df9
7ayun <wcentipede27@gmail.com>
2026-03-12 23:21:14 +0900

    Title: 특이한 출력, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260312/특이한 출력/special-print.py
@@ -0,0 +1,9 @@
+N = int(input())
+
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        if (i + j) % 4 == 0:
+            print((i, j), end = "\n")
+
+        else:
+            print((i, j), end = " ")
