60fefbc7795d62193826e3b77f705a73a5e4fb66
7ayun <wcentipede27@gmail.com>
2026-03-12 14:41:45 +0900

    Title: 별표 출력하기 13, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260312/별표 출력하기 13/print-start-13.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+for i in range(N, 0, -1):
+    for _ in range(i):
+        print("*", end = " ")
+    print()
+    
+    for _ in range(N + 1 - (i * 1)):
+        print("*", end = " ")
+    print()
