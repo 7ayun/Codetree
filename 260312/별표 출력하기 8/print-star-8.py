15dbdae7af8b19bc2ef6bc50e50791558fa4882d
7ayun <wcentipede27@gmail.com>
2026-03-12 14:19:36 +0900

    Title: 별표 출력하기 8, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260312/별표 출력하기 8/print-star-8.py
+++ 260312/별표 출력하기 8/print-star-8.py
@@ -2,9 +2,8 @@ N = int(input())
 
 for i in range(1, N + 1):
     if i % 2 != 0:
-        print("*", end = " ")
+        print("*")
     
     elif i % 2 == 0:
-        print("* " * i, end = " ")
+        print("* " * i)
     
-    print()
