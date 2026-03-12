6d22add9501c277f133761d562a35715acd9a194
7ayun <wcentipede27@gmail.com>
2026-03-12 14:12:41 +0900

    Title: 체크 다이아몬드, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260312/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+for i in range(N):
+    for _ in range(N - 1 - i):
+        print(" ", end = "")
+    for _ in range(i + 1):
+        print("*", end = " ")
+    print()
+    
+for i in range(N - 1):
+    for _ in range(i + 1):
+        print(" ", end = "")
+    for _ in range(N - 1 - i):
+        print("*", end = " ")
+    print()
