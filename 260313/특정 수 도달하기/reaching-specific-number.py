14078de83619dca0205637540e0acb17ed5a795d
7ayun <wcentipede27@gmail.com>
2026-03-13 16:36:22 +0900

    Title: 특정 수 도달하기, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260313/특정 수 도달하기/reaching-specific-number.py
@@ -0,0 +1,15 @@
+nums = list(map(int, input().split()))
+
+cnt = 0
+total = 0
+
+for num in nums:
+    if num >= 250:
+        break
+
+    total += num
+    cnt += 1
+
+avg = (total / cnt)
+
+print(total, avg)
